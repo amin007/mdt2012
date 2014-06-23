ee2f749238c53faf99bac7a9834cc1a66e3356da
Amin007 - Server <amin800507@gmail.com>
2014-06-23 15:44:38 +0800

    bagi komen

--- sesi.php
+++ sesi.php
@@ -10,14 +10,14 @@ $_SESSION[status] = $medan[status];
 */
 date_default_timezone_set('Asia/Kuala_Lumpur');
 session_start();
+# setkan session
 	$nama_anda = (isset($_SESSION['user']) ) ? $_SESSION['user'] : null;
 	$pass_anda = (isset($_SESSION['pass']) ) ? $_SESSION['pass'] : null;
 	$level_anda = (isset($_SESSION['level']) ) ? $_SESSION['level'] : null;
 	$sql =(isset($_SESSION['sql']) ) ? $_SESSION['sql'] : null;
 	$rekod = (isset($_SESSION['rekod']) ) ? $_SESSION['rekod'] : null;
 	$jumlah = (isset($_SESSION['jumlah']) ) ? $_SESSION['jumlah'] : null;
-
-
+# semak session
 if($_SESSION['user']==null): header('Location:../logout.php'); endif; 
 $masaGunaSistem = 28800; // 1 jam = 3600 = 60 saat X 60 minit
 $masaTinggal = time() - $_SESSION['tamatXtvt'];
